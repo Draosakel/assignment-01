0cdf21247637f9cc3ec91286f729c4431641b05a
JonasUJ <jonas@ulsnes.dk>
2022-09-14 13:03:52 +0200

    Add Urls

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -97,9 +97,38 @@ public class RegExprTests
     public void Urls()
     {
         // Arrange
+        var input = "<div><p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+        var expected = new List<(Uri, string)>
+        {
+            (new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"),
+            (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "Formal language"),
+            (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"),
+            (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"),
+            (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"),
+            (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)"),
+        };
 
         // Act
+        var actual = RegExpr.Urls(input);
 
         // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Urls_missing_title()
+    {
+        // Arrange
+        var input = "<a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\">TESTTEST</a>"; 
+        var expected = new List<(Uri, string)>
+        {
+            (new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "TESTTEST"),
+        };
+
+        // Act
+        var actual = RegExpr.Urls(input);
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -26,13 +26,31 @@ public static class RegExpr
         }
     }
 
+    private static string InnerTextSingle(string html, string tag)
+    {
+        return Regex.Replace(html, @$"(<({tag})[^>]*>)([^<]*)(<\/\2>)", m => m.Groups[3].Value);
+    }
+
     public static IEnumerable<string> InnerText(string html, string tag)
     {
-        foreach (Match match in Regex.Matches(html, @$"<{tag}[^>]*>(.*?)<\/{tag}>"))
+        foreach (Match match in Regex.Matches(html, @$"<{tag}[\S\s]*?>(.*?)<\/{tag}>"))
         {
-            yield return Regex.Replace(match.Groups[1].Value, @$"(<((?!{tag})\w+)[^>]*>)([^<]*)(<\/\2>)", m => m.Groups[3].Value);
+            yield return InnerTextSingle(match.Groups[1].Value, @$"(?!{tag})\w+");
         }
     }
 
-    public static IEnumerable<(Uri url, string title)> Urls(string html) => throw new NotImplementedException();
+    public static IEnumerable<(Uri url, string title)> Urls(string html)
+    {
+        foreach (Match match in Regex.Matches(html, @"<a(title=""(?<title>.*?)""|href=""(?<href>.*?)""|[\S\s])*?>(?<inner>.*?)<\/a>"))
+        {
+            var uri = new Uri(match.Groups["href"].Value);
+            if (match.Groups["title"].Success)
+            {
+                yield return (uri, match.Groups["title"].Value);
+            } else
+            {
+                yield return (uri, InnerTextSingle(match.Groups[0].Value, "a"));
+            }
+        }
+    }
 }
